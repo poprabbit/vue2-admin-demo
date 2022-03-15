15eb836829c074ae209bbdca1314d9b289268c71
wanghuanyan <wanghuanyan@xiongmaitech.com>
2022-03-15 18:31:33 +0800

    测试提交

--- src/views/UserManage/user.vue
+++ src/views/UserManage/user.vue
@@ -1,14 +1,6 @@
 <template>
-  <el-table
-    :data="tableData"
-    border
-    style="width: 100%">
-    <el-table-column
-      fixed
-      prop="date"
-      label="日期"
-      width="150">
-    </el-table-column>
+  <el-table :data="tableData" border style="width: 100%">
+    <el-table-column fixed prop="date" label="日期" width="150"></el-table-column>
     <el-table-column
       prop="name"
       label="姓名"
